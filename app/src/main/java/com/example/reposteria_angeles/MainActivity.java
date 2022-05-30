4471ba19d48b4b922975be5c52fc3fcb3fcaf7f4
mdmiguel <104116693+retomike@users.noreply.github.com>
2022-05-30 11:29:45 -0500

    Vista Gastos
    Se finalizó el diseño de gastos

--- app/src/main/java/com/example/reposteria_angeles/MainActivity.java
+++ app/src/main/java/com/example/reposteria_angeles/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void iniciarSesion(View view){
-        Intent intent = new Intent(MainActivity.this, VentaActivity.class);
+        Intent intent = new Intent(MainActivity.this, GastosActivity.class);
         startActivity(intent);
     }
 }
