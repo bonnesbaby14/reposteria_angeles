29a6e8731e26a37ce5bf45d9be66eda40ae69177
mdmiguel <104116693+retomike@users.noreply.github.com>
2022-06-08 22:41:48 -0500

    Se agrego estadísticas
    implementation 'com.github.PhilJay:MPAndroidChart:v3.1.0'
    
     maven { url "https://jitpack.io" }

--- app/src/main/java/com/example/reposteria_angeles/ui/estadistica/EstadisticaFragment.java
+++ app/src/main/java/com/example/reposteria_angeles/ui/estadistica/EstadisticaFragment.java
@@ -1,6 +1,7 @@
-package com.example.reposteria_angeles.ui.reporte;
+package com.example.reposteria_angeles.ui.estadistica;
 
 import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,23 +13,33 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.reposteria_angeles.R;
-import com.example.reposteria_angeles.databinding.FragmentReporteBinding;
+import com.example.reposteria_angeles.databinding.FragmentEstadisticaBinding;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 
-public class ReporteFragment extends Fragment implements View.OnClickListener {
+public class EstadisticaFragment extends Fragment implements View.OnClickListener {
 
-        private FragmentReporteBinding binding;
+        private FragmentEstadisticaBinding binding;
         //Componentes del Fragment
         TextView txtInicio, txtFinal;
         private int dia, mes, anio;
+        ArrayList barArrayList;
 
         public View onCreateView(@NonNull LayoutInflater inflater,
                                  ViewGroup container, Bundle savedInstanceState) {
 
 
-            binding = FragmentReporteBinding.inflate(inflater, container, false);
+            binding = FragmentEstadisticaBinding.inflate(inflater, container, false);
             View root = binding.getRoot();
 
 
@@ -36,7 +47,26 @@ public class ReporteFragment extends Fragment implements View.OnClickListener {
             txtInicio.setOnClickListener(this);
             txtFinal = (TextView) root.findViewById(R.id.txtFinalEstadistica);
             txtFinal.setOnClickListener(this);
+            BarChart barChart = root.findViewById(R.id.barChart);
+            getData();
+            BarDataSet barDataSet = new BarDataSet(barArrayList, "Estadísticas");
+            BarData barData = new BarData (barDataSet);
+            barChart.setData(barData);
+            barDataSet.setColors(ColorTemplate.PASTEL_COLORS);
+            barDataSet.setValueTextColor(Color.BLACK);
+            barDataSet.setValueTextSize(8f);
+            //barChart.getDescription().setEnabled(true);
+
             return root;
+        }
+
+        private void getData(){
+            barArrayList = new ArrayList();
+            barArrayList.add(new BarEntry(2f, 3));
+            barArrayList.add(new BarEntry(4f, 2));
+            barArrayList.add(new BarEntry(6f, 12));
+            barArrayList.add(new BarEntry(8f, 5));
+            barArrayList.add(new BarEntry(10f, 10));
         }
 
         @Override
