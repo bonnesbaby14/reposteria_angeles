5f725ebcc3da0edc1dfe8620bb82581ccb0d2853
RafaRG21 <rafaruiz5269@gmail.com>
2022-06-13 22:24:52 -0500

    Reporte Total

--- app/src/main/java/com/example/reposteria_angeles/ui/reporte/ReporteFragment.java
+++ app/src/main/java/com/example/reposteria_angeles/ui/reporte/ReporteFragment.java
@@ -50,7 +50,7 @@ public class ReporteFragment extends Fragment implements View.OnClickListener {
         private FragmentReporteBinding binding;
         //Componentes del Fragment
         TextView txtInicio, txtFinal;
-        Button generar;
+        Button generar, reporteTotal;
     String puntero="";
     Spinner spinner;
     ArrayList<String> opciones;
@@ -98,17 +98,17 @@ public class ReporteFragment extends Fragment implements View.OnClickListener {
             txtFinal.setOnClickListener(this);
 
             generar=root.findViewById(R.id.btnReporteReporte);
-
+            reporteTotal = root.findViewById(R.id.btnIngresosReporte);
             generar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
                     if(puntero.equals("") || puntero.equals("Selecciona")){
-                        Toast.makeText(getContext(),"Seleciona una entidad",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getContext(),"Selecciona una entidad",Toast.LENGTH_LONG).show();
                         return;
                     }
                     if(txtFinal.getText().toString().isEmpty()|| txtInicio.getText().toString().isEmpty()){
-                        Toast.makeText(getContext(),"Seleciona fechas",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getContext(),"Selecciona fechas",Toast.LENGTH_LONG).show();
                         return;
                     }
 
@@ -124,9 +124,25 @@ public class ReporteFragment extends Fragment implements View.OnClickListener {
                 }
             });
 
+            reporteTotal.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(txtFinal.getText().toString().isEmpty()|| txtInicio.getText().toString().isEmpty()){
+                        Toast.makeText(getContext(),"Selecciona fechas",Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    try {
+                        generarReporteTotal(txtInicio.getText().toString(), txtFinal.getText().toString());
+                    }catch (ParseException e){
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+
 
             return root;
-        }
+        }//reporteTotal
 
         @Override
         public void onDestroyView() {
@@ -192,6 +208,8 @@ public class ReporteFragment extends Fragment implements View.OnClickListener {
         titulo.setTextSize(30);
 
         canvas.drawText("Reporte de "+puntero, 10, 150, titulo);
+        titulo.setTextSize(20);
+        canvas.drawText("Del "+fecha1+" al "+fecha2, 10, 180, titulo);
 
         descripcion.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
         descripcion.setTextSize(15);
@@ -387,6 +405,229 @@ public class ReporteFragment extends Fragment implements View.OnClickListener {
 
 
     }//generarTicket
+    private void generarReporteTotal(String fecha1, String fecha2) throws ParseException{
+        Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(fecha1);
+        Date date2=new SimpleDateFormat("dd/MM/yyyy").parse(fecha2);
+
+        PdfDocument pdfDocument=new PdfDocument();
+        Paint paint=new Paint();
+        TextPaint titulo=new TextPaint();
+        TextPaint descripcion = new TextPaint();
+        Bitmap bitmap,bitmapEscala;
+
+        PdfDocument.PageInfo pageInfo=new PdfDocument.PageInfo.Builder(750,700,3).create();
+        PdfDocument.Page pagina= pdfDocument.startPage(pageInfo);
+        Canvas canvas=pagina.getCanvas();
+        bitmap= BitmapFactory.decodeResource(getResources(),R.mipmap.logo);
+        bitmapEscala =Bitmap.createScaledBitmap(bitmap,80,80,false);
+        canvas.drawBitmap(bitmapEscala, 50, 20, paint);
+        titulo.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        titulo.setTextSize(30);
+
+        canvas.drawText("Reporte Total", 10, 150, titulo);
+        titulo.setTextSize(20);
+        canvas.drawText("Del "+fecha1+" al "+fecha2, 10, 180, titulo);
+        descripcion.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+        descripcion.setTextSize(15);
+
+        int y = 210;
+        boolean hayDatos = false;
+        //Productos
+        //titulo
+        titulo.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        titulo.setTextSize(20);
+        canvas.drawText("Productos", 5, y, titulo);
+        y+=20;
+        canvas.drawText("NOMBRE    CANTIDAD    PRECIO    CADUCIDAD     DESCRIPCION", 5, y, descripcion);
+        y+=20;
+
+        try {
+            InputStreamReader archivo = new InputStreamReader(getContext().openFileInput("productos.txt"));
+            BufferedReader br = new BufferedReader(archivo);
+            String linea = br.readLine();
+
+            while (linea != null) {
+
+                String[] aux = linea.split("-");
+                Date date=new SimpleDateFormat("dd/MM/yyyy").parse(aux[3]);
+                if(date.after(date1) && date.before(date2)){
+                    hayDatos = true;
+                    aux[0] = espacioCampos("nombre",aux[0]);
+                    aux[1] = espacioCampos("cantidad",aux[1]);
+                    aux[2] = espacioCampos("precio",aux[2]);
+                    aux[3] = espacioCampos("caducidad",aux[3]);
+                    aux[4] = espacioCampos("descripcion",aux[4]);
+
+
+                    canvas.drawText(aux[0]+"              "+aux[1]+"          "+aux[2]+"          "+aux[3]+
+                            "        "+aux[4], 5, y, descripcion);
+                    y+=20;
+                }
+
+                linea = br.readLine();
+            }
+
+            br.close();
+            archivo.close();
+
+
+        } catch (IOException e) {
+
+        }
+        //Ingresos
+        //titulo
+        titulo.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        titulo.setTextSize(20);
+        canvas.drawText("Ingresos", 5, y, titulo);
+        y+=20;
+        canvas.drawText("CLIENTE    PRODUCTOS    VENTA TOTAL    NOMBRE VENTA     PRODUCTOS VENDIDOS    DESCRIPCION", 5, y, descripcion);
+        y+=20;
+        try {
+            InputStreamReader archivo = new InputStreamReader(getContext().openFileInput("ingresos.txt"));
+            BufferedReader br = new BufferedReader(archivo);
+            String linea = br.readLine();
+
+            while (linea != null) {
+
+                String[] aux = linea.split("-");
+                Date date=new SimpleDateFormat("dd/MM/yyyy").parse(aux[6]);
+                if(date.after(date1) && date.before(date2)){
+                    hayDatos = true;
+                    aux[0] = espacioCampos("CLIENTE",aux[0]);
+                    aux[1] = espacioCampos("PRODUCTOS",aux[1]);
+                    aux[2] = espacioCampos("VENTA TOTAL",aux[2]);
+                    aux[3] = espacioCampos("NOMBRE VENTA",aux[3]);
+                    aux[4] = espacioCampos("PRODUCTOS VENDIDOS",aux[4]);
+                    aux[5] = espacioCampos("DESCRIPCION",aux[5]);
+
+                    canvas.drawText(aux[0]+"           "+aux[1]+"               "+aux[2]+"            "+aux[3]+
+                            "           "+aux[4]+"               "+aux[5], 5, y, descripcion);
+                    y+=20;
+                }
+
+                linea = br.readLine();
+            }
+
+            br.close();
+            archivo.close();
+
+
+        } catch (IOException e) {
+
+        }
+        //gastos
+        //titulo
+        titulo.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        titulo.setTextSize(20);
+        canvas.drawText("Gastos", 5, y, titulo);
+        y+=20;
+        canvas.drawText("PRODUCTOS          NOMBRE          COSTO        PRODUCTOS NUEVOS         DESCRIPCION", 5, y, descripcion);
+        y+=20;
+
+        try {
+            InputStreamReader archivo = new InputStreamReader(getContext().openFileInput("gastos.txt"));
+            BufferedReader br = new BufferedReader(archivo);
+            String linea = br.readLine();
+
+            while (linea != null) {
+
+                String[] aux = linea.split("-");
+                Date date=new SimpleDateFormat("dd/MM/yyyy").parse(aux[5]);
+                if(date.after(date1) && date.before(date2)){
+                    hayDatos = true;
+                    aux[0] = espacioCampos("PRODUCTOS",aux[0]);
+                    aux[1] = espacioCampos("NOMBRE",aux[1]);
+                    aux[2] = espacioCampos("COSTO",aux[2]);
+                    aux[3] = espacioCampos("PRODUCTOS NUEVOS",aux[3]);
+                    aux[4] = espacioCampos("DESCRIPCION",aux[4]);
+
+
+                    canvas.drawText(aux[0]+"                "+aux[1]+"             "+aux[2]+"                    "+aux[3]+
+                            "                        "+aux[4], 5, y, descripcion);
+                    y+=20;
+                }
+
+                linea = br.readLine();
+            }
+
+            br.close();
+            archivo.close();
+
+
+        } catch (IOException e) {
+
+        }
+        //clientes
+        //titulo
+        titulo.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        titulo.setTextSize(20);
+        canvas.drawText("Clientes", 5, y, titulo);
+        y+=20;
+        canvas.drawText("NOMBRE               DIRECCIÓN               TELÉFONO               PREFERENCIAS", 5, y, descripcion);
+        y+=20;
+
+        try {
+            InputStreamReader archivo = new InputStreamReader(getContext().openFileInput("clientes.txt"));
+            BufferedReader br = new BufferedReader(archivo);
+            String linea = br.readLine();
+
+            while (linea != null) {
+
+                String[] aux = linea.split("-");
+                Date date=new SimpleDateFormat("dd/MM/yyyy").parse(aux[4]);
+                if(date.after(date1) && date.before(date2)){
+                    hayDatos = true;
+                    aux[0] = espacioCampos("NOMBRE",aux[0]);
+                    aux[1] = espacioCampos("DIRECCIÓN",aux[1]);
+                    aux[2] = espacioCampos("TELÉFONO",aux[2]);
+                    aux[3] = espacioCampos("PREFERENCIAS",aux[3]);
+
+
+                    canvas.drawText(aux[0]+"                   "+aux[1]+"                "+aux[2]+"                       "+aux[3], 5, y, descripcion);
+                    y+=20;
+                }
+
+                linea = br.readLine();
+            }
+
+            br.close();
+            archivo.close();
+
+
+        } catch (IOException e) {
+
+        }
+        pdfDocument.finishPage(pagina);
+
+        try {
+            if(!hayDatos){
+                Toast.makeText(getContext(), "No hay registros en estas fechas", Toast.LENGTH_LONG).show();
+
+            }else {
+                Log.d("FILES", "try");
+                ContextWrapper contextWrapper = new ContextWrapper(getContext());
+                File directory = contextWrapper.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+                int num = 1;
+                puntero = "Total";
+                String nameDocument = "Reporte_" + puntero + "_" + num + ".pdf";
+                File file = new File(directory, nameDocument);
+                file = nombreArchivo(file, nameDocument, directory);
+
+
+                pdfDocument.writeTo(new FileOutputStream(file));
+
+                Toast.makeText(getContext(), "Se creo el PDF correctamente en " + directory.toString(), Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Log.d("FILES", "Error escritura de archivo");
+
+            Log.d("FILES", e.toString());
+            e.printStackTrace();
+        }
+
+        pdfDocument.close();
+
+    }//generarReporteTotal
     private String espacioCampos(String campo, String dato){
         if(dato.length()<campo.length()){
                 for(int i=0;i<campo.length()-dato.length();i++){
